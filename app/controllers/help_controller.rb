74f88b233c3cfbe7bd9dd99dc4358bf1a4b03676
pentestmonkey <pentestmonkey@d6118428-16fc-11de-b091-d144e675ee08>
2010-04-02 22:58:15 +0000

    Adding SVN Revision number of version number: svn propset svn:keywords "Revision Date Id" app/controllers/help_controller.rb

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -2,14 +2,22 @@ class HelpController < ApplicationController
   # GET /help
   # GET /help.xml
   def index 
+  svn = "$Revision$".gsub(/[^0-9]/, "")
   @yaptestfe_version="1.1-rc4"
+  if not svn =~ /^\s*$/
+  	@yaptestfe_version = @yaptestfe_version + "-svn-" + svn
+  end
     respond_to do |format|
       format.html # index.html.erb
     end
   end
 
   def show
+  svn = "$Revision$".gsub(/[^0-9]/, "")
   @yaptestfe_version="1.1-rc4"
+  if not svn =~ /^\s*$/
+  	@yaptestfe_version = @yaptestfe_version + "-svn-" + svn
+  end
     respond_to do |format|
       format.html # show.html.erb
     end
