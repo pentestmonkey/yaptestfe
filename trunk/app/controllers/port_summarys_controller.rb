eb395313227030251f7f67d887a022d155749525
pentestmonkey <pentestmonkey@d6118428-16fc-11de-b091-d144e675ee08>
2012-03-03 18:41:34 +0000

    Bug fix: Port Summary page can filter by test area.  You will need to update your yaptest schema (fix in svn already).  Also fixed yaptestfe -W - lowercases database name for you to avoid errors.

--- trunk/app/controllers/port_summarys_controller.rb
+++ trunk/app/controllers/port_summarys_controller.rb
@@ -35,6 +35,8 @@ class PortSummarysController < ApplicationController
 
         order_clause = "#{@sort_by} #{@sort_dir}"
       end
+   else
+     order_clause = "count desc"
    end
 
    if not params[:trans_v].nil?
@@ -42,11 +44,21 @@ class PortSummarysController < ApplicationController
      @url_params[:trans_v] = params[:trans_v]
    end
 
-  @record_count = PortSummary.count
-  @pager = Paginator.new(@record_count, session[:tablen]) do |offset, per_page|
-    PortSummary.find(:all, :order => order_clause, :limit => per_page, :offset => offset, :conditions => where)
-    # TODO order by something
+  if session[:test_area].nil?
+    @record_count = PortSummary.find(:all, :conditions => where).length
+    @pager = Paginator.new(@record_count, session[:tablen]) do |offset, per_page|
+      # PortSummary.find(:all, :order => order_clause, :limit => per_page, :offset => offset, :conditions => where)
+      PortSummary.find(:all, :select => ["sum(count) as count, port, transport_protocol"], :order => order_clause, :limit => per_page, :offset => offset, :conditions => where, :group => "port, transport_protocol")
+      # TODO order by something
+    end
+  else
+    @record_count = PortSummary.find_all_by_test_area_id(session[:test_area], :conditions => where).length
+    @pager = Paginator.new(@record_count, session[:tablen]) do |offset, per_page|
+      PortSummary.find_all_by_test_area_id(session[:test_area], :order => order_clause, :limit => per_page, :offset => offset, :conditions => where)
+      # TODO order by something
+    end
   end
+
   @port_summarys = @pager.page(params[:page])
 
     respond_to do |format|
